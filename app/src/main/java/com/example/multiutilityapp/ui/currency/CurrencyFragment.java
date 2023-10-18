53fb2a44feaba8431e0a477679888a4ccd9c211d
Suraj Shinde <surajshinde2509@gmail.com>
2023-10-18 18:19:28 +0530

    Third Running "Currency Converter Final"

--- app/src/main/java/com/example/multiutilityapp/ui/currency/CurrencyFragment.java
+++ app/src/main/java/com/example/multiutilityapp/ui/currency/CurrencyFragment.java
@@ -1,38 +1,94 @@
 package com.example.multiutilityapp.ui.currency;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import androidx.fragment.app.Fragment;
 
 import com.example.multiutilityapp.R;
 
 public class CurrencyFragment extends Fragment {
 
-    private CurrencyViewModel mViewModel;
+    private EditText amountEditText;
+    private Spinner currencySpinner;
+    private Button convertButton;
+    private TextView resultTextView;
 
-    public static CurrencyFragment newInstance() {
-        return new CurrencyFragment();
-    }
+
+    private static final double IND_TO_USD_RATE = 0.012,
+            IND_TO_CD_RATE = 0.016, IND_TO_CY_RATE = 0.088, IND_TO_EURO_RATE = 0.011,
+            IND_TO_HKD_RATE = 0.094, IND_TO_SKW_RATE = 16.23, IND_TO_RR_RATE = 1.17,
+            IND_TO_JY_RATE = 1.80, IND_TO_SD_RATE = 0.016, IND_TO_SLR_RATE = 3.90,
+            IND_TO_PR_RATE = 3.36, IND_TO_UAED_RATE = 0.044;
+
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_currency, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_currency, container, false);
+
+        amountEditText = view.findViewById(R.id.amountEditText);
+        currencySpinner = view.findViewById(R.id.currencySpinner);
+        convertButton = view.findViewById(R.id.convertButton);
+        resultTextView = view.findViewById(R.id.resultTextView);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                getActivity(), R.array.currencies, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        currencySpinner.setAdapter(adapter);
+
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                convertCurrency();
+            }
+        });
+
+        return view;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(CurrencyViewModel.class);
-        // TODO: Use the ViewModel
-    }
+    private void convertCurrency() {
+        String amountString = amountEditText.getText().toString();
+        if (!amountString.isEmpty()) {
+            double amount = Double.parseDouble(amountString);
+            String selectedCurrency = currencySpinner.getSelectedItem().toString();
 
+            double convertedAmount = 0;
+            if (selectedCurrency.equals("United States Dollar")) {
+                convertedAmount = amount * IND_TO_USD_RATE;
+            } else if (selectedCurrency.equals("Canadian Dollar")) {
+                convertedAmount = amount * IND_TO_CD_RATE;
+            }else if (selectedCurrency.equals("Chinese Yuan")) {
+                convertedAmount = amount * IND_TO_CY_RATE;
+            }else if (selectedCurrency.equals("Euro")) {
+                convertedAmount = amount * IND_TO_EURO_RATE;
+            }else if (selectedCurrency.equals("Hong Kong Dollar")) {
+                convertedAmount = amount * IND_TO_HKD_RATE;
+            }else if (selectedCurrency.equals("South Korean won")) {
+                convertedAmount = amount * IND_TO_SKW_RATE;
+            }else if (selectedCurrency.equals("Russian Ruble")) {
+                convertedAmount = amount * IND_TO_RR_RATE;
+            }else if (selectedCurrency.equals("Japanese Yen")) {
+                convertedAmount = amount * IND_TO_JY_RATE;
+            }else if (selectedCurrency.equals("Singapore Dollar")) {
+                convertedAmount = amount * IND_TO_SD_RATE;
+            }else if (selectedCurrency.equals("Sri Lankan Rupee")) {
+                convertedAmount = amount * IND_TO_SLR_RATE;
+            }else if (selectedCurrency.equals("Pakistan Rupee")) {
+                convertedAmount = amount * IND_TO_PR_RATE;
+            }else if (selectedCurrency.equals("UAE Dirham")) {
+                convertedAmount = amount * IND_TO_UAED_RATE;
+            }
+
+            resultTextView.setText(String.format("%.2f IND Rupee = %.2f %s", amount, convertedAmount, selectedCurrency));
+        } else {
+            resultTextView.setText("Enter valid amount.");
+        }
+    }
 }
--- app/src/main/java/com/example/multiutilityapp/ui/currency/CurrencyViewModel.java
+++ app/src/main/java/com/example/multiutilityapp/ui/currency/CurrencyViewModel.java
@@ -1,7 +1,56 @@
 package com.example.multiutilityapp.ui.currency;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.lifecycle.ViewModel;
 
 public class CurrencyViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+//    public CurrencyViewModel(){
+//        public void usd(View view) {
+//            EditText INR = (EditText)findViewById(R.id.INR);
+//            float Inr = Float.parseFloat(INR.getText().toString());
+//            double out = Inr / (76.01);
+//            Toast.makeText(this ,"Your currency value is " + out , Toast.LENGTH_LONG).show();
+//        }
+//
+//        public void aed(View view) {
+//            EditText INR = (EditText)findViewById(R.id.INR);
+//            float Inr = Float.parseFloat(INR.getText().toString());
+//            double out = Inr / (20.78);
+//            Toast.makeText(this ,"Your currency value is " + out , Toast.LENGTH_LONG).show();
+//        }
+//
+//        public void pound(View view) {
+//            EditText INR = (EditText)findViewById(R.id.INR);
+//            float Inr = Float.parseFloat(INR.getText().toString());
+//            double out = Inr / (100.69);
+//            Toast.makeText(this ,"Your currency value is " + out , Toast.LENGTH_LONG).show();
+//        }
+//
+//        public void jyen(View view) {
+//            EditText INR = (EditText)findViewById(R.id.INR);
+//            float Inr = Float.parseFloat(INR.getText().toString());
+//            double out = Inr / (0.67);
+//            Toast.makeText(this ,"Your currency value is " + out , Toast.LENGTH_LONG).show();
+//        }
+//
+//        public void tbht(View view) {
+//            EditText INR = (EditText)findViewById(R.id.INR);
+//            float Inr = Float.parseFloat(INR.getText().toString());
+//            double out = Inr / (2.28);
+//            Toast.makeText(this ,"Your currency value is " + out , Toast.LENGTH_LONG).show();
+//        }
+//
+//        public void candollar(View view) {
+//            EditText INR = (EditText)findViewById(R.id.INR);
+//            float Inr = Float.parseFloat(INR.getText().toString());
+//            double out = Inr / (58.95);
+//            Toast.makeText(this ,"Your currency value is " + out , Toast.LENGTH_LONG).show();
+//        }
+//    }
+
 }
