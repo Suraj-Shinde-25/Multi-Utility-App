fea3b1e6319c40f261eece29a54e4f246d2cf7d3
Suraj Shinde <surajshinde2509@gmail.com>
2023-10-22 18:47:20 +0530

    Six Running "Currency Converter, Calculator, Home Final"

--- app/src/main/java/com/example/multiutilityapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/multiutilityapp/ui/home/HomeFragment.java
@@ -10,28 +10,14 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.multiutilityapp.R;
 import com.example.multiutilityapp.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 }
