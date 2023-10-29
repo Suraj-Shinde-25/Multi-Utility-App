2a17dc14978cded44af2f5a16bc5e12be40305e4
Suraj Shinde <surajshinde2509@gmail.com>
2023-10-29 14:57:57 +0530

    11 Running ? "Image ZOOM"

--- app/src/main/java/com/example/multiutilityapp/ui/calender/CalenderActivity.java
+++ app/src/main/java/com/example/multiutilityapp/ui/calender/CalenderActivity.java
@@ -1,17 +1,22 @@
 package com.example.multiutilityapp.ui.calender;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.View;
 import com.example.multiutilityapp.R;
 
 public class CalenderActivity extends AppCompatActivity {
-
+    private ScaleGestureDetector scaleGestureDetector;
+    private float FACTOR = 1.0f;
     TextView textView;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +34,25 @@ public class CalenderActivity extends AppCompatActivity {
         textView = (TextView)findViewById(R.id.monthName);
         textView.setText(receivedName);
 
-        ImageView imageView = (ImageView) findViewById(R.id.monthImage);
+        imageView = (ImageView) findViewById(R.id.monthImage);
         imageView.setImageResource(receivedImage);
+        scaleGestureDetector = new ScaleGestureDetector(this,new ScaleListner());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        scaleGestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class ScaleListner extends ScaleGestureDetector.SimpleOnScaleGestureListener{
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            FACTOR *= detector.getScaleFactor();
+            FACTOR = Math.max(0.1f, Math.min(FACTOR, 10.f));
+            imageView.setScaleX(FACTOR);
+            imageView.setScaleY(FACTOR);
+            return true;
+        }
     }
 }
